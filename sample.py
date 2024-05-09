faaa9e7ad93aac1e2997c1f5ac39739709fd493a
Archana <165228454+Archanaumashankar@users.noreply.github.com>
2024-05-09 18:40:01 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,68 @@
+import calendar
+def print_calender(year, month):
+    cal =calendar.month (year,month)
+    return cal
+
+year=2023
+month=1
+output = print_calender(year, month)
+print(output)
+
+year =2023
+month=2
+output= print_calender(year,month)
+print(output)
+
+year=2023
+month=3
+output = print_calender(year,month)
+print(output)
+
+year=2023
+month=4
+output = print_calender(year,month)
+print(output)
+
+year=2023
+month=5
+output = print_calender(year,month)
+print(output)
+
+year=2023
+month=6
+output = print_calender(year,month)
+print(output)
+
+year=2023
+month=7
+output = print_calender(year,month)
+print(output)
+
+year=2023
+month=8
+output= print_calender(year,month)
+print(output)
+
+year=2023
+month=9
+output= print_calender(year,month)
+print(output)
+
+year=2023
+month=10
+output= print_calender(year,month)
+print(output)
+
+year=2023
+month=11
+output=print_calender(year,month)
+print(output)
+
+year=2023
+month=12
+output=print_calender(year,month)
+print(output)
+
+
+
+
